139bb0241ab659f6151a844870652aeed06a2c61
queses <quesesr@gmail.com>
2015-12-26 10:40:31 +0500

    C6
    Доработка frontend

--- application/controllers/board.py
+++ application/controllers/board.py
@@ -4,8 +4,9 @@ from application.models import board
 
 def __return__():
     return render_template('board/board.html', 
-        lecturers = board.Lecturer.query.all(), disciplines = board.Discipline.query.all(),)
+        lecturers = board.Lecturer.query.all(), disciplines = board.Discipline.query.all())
 
 def __return_modal__(id):
     lecturer = board.Lecturer.query.get(id)
+    print("esdasd"+lecturer.description)
     return render_template("board/modal.html", lecturer = lecturer)
